7802ab5dc889f58e85dbfcc90fcb5b0fe4b72ae2
afroukh99 <mohammedtea22@gmail.com>
2024-02-05 10:39:55 +0100

    js changes

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const section = document.querySelectorAll("section")
 const icon = document.querySelectorAll(".icon")
 const desc = document.querySelectorAll(".desc")
 
+
+
+
 section.forEach((ele, index) => {
     let clicked = true;
-    const img = ele.children[0].children[1]
-    img.addEventListener("click", () => {
+    const header = ele.children[0]
+    const img = header.children[1]
+    header.addEventListener("click", () => {
         if (!clicked) {
             img.setAttribute("src", "assets/images/icon-plus.svg")
             clicked = true
